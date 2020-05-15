169026fffaa68d8b12e9e36df0673308d051e55a
kstasi <nastasiakondaurova@gmail.com>
2020-05-15 21:38:00 +0300

    prometheus connection fixed

--- getaway/src/main/java/ua/akondaur/ClientApplication.java
+++ getaway/src/main/java/ua/akondaur/ClientApplication.java
@@ -3,11 +3,15 @@ package ua.akondaur;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
 
 @EnableFeignClients
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableRetry
+@EnableCircuitBreaker
 public class ClientApplication {
 
 	public static void main(String[] args) {
