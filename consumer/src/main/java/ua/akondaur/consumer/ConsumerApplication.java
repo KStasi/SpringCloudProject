626ef6b5343ec141e2c837c2b775d9342a32417a
kstasi <nastasiakondaurova@gmail.com>
2020-05-16 08:12:39 +0300

    kafka consumer service draft

---
+++ consumer/src/main/java/ua/akondaur/consumer/ConsumerApplication.java
@@ -0,0 +1,19 @@
+package ua.akondaur.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApplication.class, args);
+
+		try {
+		   KafkaConsumerDemo.runConsumer();
+		} catch(Exception e) {
+		   System.out.println(e);
+		}
+	}
+
+}
---
+++ consumer/src/main/java/ua/akondaur/consumer/KafkaConsumerDemo.java
@@ -0,0 +1,48 @@
+package ua.akondaur.consumer;
+
+import java.util.Properties;
+import java.util.Collections;
+
+import java.util.Arrays;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+
+public class KafkaConsumerDemo {
+    private final static String TOPIC = "create.entity";
+    private final static String BOOTSTRAP_SERVERS = "kafka:9092";
+ 
+    private static Consumer<String, String> createConsumer() {
+        final Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "KafkaConsumer");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        final Consumer<String, String> consumer =
+                new KafkaConsumer<>(props);
+ 
+        consumer.subscribe(Collections.singletonList(TOPIC));
+        return consumer;
+    }
+
+   public static void runConsumer() {
+
+        final Consumer<String, String> consumer = createConsumer();
+        try {
+            while (true) {
+
+                ConsumerRecords<String, String> records = consumer.poll(100);
+
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.printf("Consumer Record:(%s, %s, %d, %d)\n",
+                            record.key(), record.value(),
+                            record.partition(), record.offset());
+                }
+            }
+            } finally {
+                consumer.close();
+            }
+        }
+}
